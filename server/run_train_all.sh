a72c16c3704b897974ab0a41d1b7f4a121109607
matt clifford <matt.clifford@bristol.ac.uk>
2022-07-20 17:31:30 +0100

    run on bc4

---
+++ server/run_train_all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+dir="server/outputs/"   # you will need to make outputs directory using 'mkdir outputs'
+mkdir $dir    # comment this out if directory already made
+ram="32G"        # change these
+
+data_dir="/user/work/mc15445/summer-project"
+epochs=100
+
+job_name="pose"
+time="0-2:00"
+
+task="edge_2d shear real"
+sbatch -t $time -J $job_name -o $dir'.out' -e $dir'.err' --mem=$ram server/submit_job.sh python trainers.py --epochs $epochs --dir $data_dir --task $task --ram
+task="edge_2d shear sim"
+sbatch -t $time -J $job_name -o $dir'.out' -e $dir'.err' --mem=$ram server/submit_job.sh python trainers.py --epochs $epochs --dir $data_dir --task $task --ram
+task="surface_2d shear real"
+sbatch -t $time -J $job_name -o $dir'.out' -e $dir'.err' --mem=$ram server/submit_job.sh python trainers.py --epochs $epochs --dir $data_dir --task $task --ram
+task="surface_2d shear sim"
+sbatch -t $time -J $job_name -o $dir'.out' -e $dir'.err' --mem=$ram server/submit_job.sh python trainers.py --epochs $epochs --dir $data_dir --task $task --ram
+
+# on dev-data
+task="edge_2d shear real"
+sbatch -t $time -J $job_name -o $dir'.out' -e $dir'.err' --mem=$ram server/submit_job.sh python trainers.py --epochs $epochs --task $task --ram
+task="surface_2d shear real"
+sbatch -t $time -J $job_name -o $dir'.out' -e $dir'.err' --mem=$ram server/submit_job.sh python trainers.py --epochs $epochs --task $task --ram
---
+++ server/submit_job.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition gpu
+
+# SBATCH --time=0:0:30
+# SBATCH --mem=64G
+# SBATCH --job-name=multi_task
+# SBATCH -e server/stderr.txt
+# SBATCH -o server/stdout.txt
+
+# #SBATCH --gres=gpu:2
+
+
+# module load libs/cuda/10.2-gcc-5.4.0-2.26
+module load languages/anaconda3/2021-3.8.8-cuda-11.1-pytorch
+# srun python run_all.py --dir /user/work/mc15445/summer-project
+
+srun $*    # run all command line inputs
+wait
+
+# srun python trainers/train_unet.py --batch_size 256 --epochs 10
+#  --dir /user/work/mc15445/summer-project
+# wait
