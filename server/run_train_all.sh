a0ff6f7f3bfb4377fa8f641bd898bc2962ab98fb
matt clifford <matt.clifford@bristol.ac.uk>
2022-07-21 10:54:33 +0100

    all

--- server/run_train_all.sh
+++ server/run_train_all.sh
@@ -23,10 +23,10 @@ name="surface_sim"
 sbatch -t $time -J $dir$name -o $dir$name'.out' -e $dir$name'.err' --mem=$ram server/submit_job.sh python trainer.py --epochs $epochs --dir $data_dir --task $task --ram
 
 # # on nathans data
-# data_dir="/user/work/mc15445/summer-project/data/Nathan/tactip-127"
-# task="edge_2d shear real"
-# name="edge_nathan"
-# sbatch -t $time -J $dir$name -o $dir$name'.out' -e $dir$name'.err' --mem=$ram server/submit_job.sh python trainer.py --epochs $epochs --dir $data_dir --task $task --ram
-# task="surface_2d shear real"
-# name="surface_nathan"
-# sbatch -t $time -J $dir$name -o $dir$name'.out' -e $dir$name'.err' --mem=$ram server/submit_job.sh python trainer.py --epochs $epochs --dir $data_dir --task $task --ram
+data_dir="/user/work/mc15445/summer-project/data/Nathan/tactip-127"
+task="edge_2d shear real"
+name="edge_nathan"
+sbatch -t $time -J $dir$name -o $dir$name'.out' -e $dir$name'.err' --mem=$ram server/submit_job.sh python trainer.py --epochs $epochs --dir $data_dir --task $task --ram
+task="surface_2d shear real"
+name="surface_nathan"
+sbatch -t $time -J $dir$name -o $dir$name'.out' -e $dir$name'.err' --mem=$ram server/submit_job.sh python trainer.py --epochs $epochs --dir $data_dir --task $task --ram
